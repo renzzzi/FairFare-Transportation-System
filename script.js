3d0414a23f8a21c6c0367971e347bc08cf1a0791
GraziellaMS21 <graziellamssaavedra06@gmail.com>
2025-04-13 23:14:12 +0800

    Major Changes in Nav Bar to make it responsive

--- script.js
+++ script.js
@@ -1,9 +1,14 @@
-function burgerHandler() {
-	let theNavBar = document.getElementsByClassName("header-nav");
+document.addEventListener("DOMContentLoaded", function () {
+	let btnburger = document.querySelector(".burger-icon");
+	let mobileNavUpper = document.querySelector(".mobile-nav.lower");
 
-	if (theNavBar.className === "header-nav") {
-		theNavBar.className += " responsive";
-	} else {
-		theNavBar.className = "header-nav";
-	}
-}
+	btnburger.addEventListener("click", function () {
+		mobileNavUpper.classList.toggle("show-menu");
+	});
+
+	window.addEventListener("resize", function () {
+		if (window.innerWidth > 1100) {
+			mobileMenu.classList.remove("show-menu");
+		}
+	});
+});
